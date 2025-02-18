57a5955b38fc8ec942e5f673641cd2d3443763d3
CristopherMartarello <crisrossi1313@gmail.com>
2025-02-18 17:52:27 -0300

    chore: add tailwind prettier plugin

--- app/transaction/page.tsx
+++ app/transaction/page.tsx
@@ -1,5 +1,9 @@
 const Transaction = () => {
-    return <h1>Opa transaction</h1>
-}
+  return (
+    <h1 className="flex w-screen items-center justify-center py-5">
+      Opa transaction
+    </h1>
+  );
+};
 
 export default Transaction;
