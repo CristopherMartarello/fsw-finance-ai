2c842850f4e1027a31372a31d8ab3e43b3e73689
CristopherMartarello <crisrossi1313@gmail.com>
2025-03-07 08:44:04 -0300

    feat: adding zod validation, react hook form and add transaction component

---
+++ app/_components/add-transaction-button.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome é obrigatório.",
+  }),
+  amount: z.string().trim().min(1, {
+    message: "O valor é obrigatório.",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório.",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatória.",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório.",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória.",
+  }),
+});
+
+const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: "",
+      category: TransactionCategory.OTHER,
+      date: new Date(),
+      name: "",
+      paymentMethod: TransactionPaymentMethod.CASH,
+      type: TransactionType.DEPOSIT,
+    },
+  });
+  console.log(form);
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="rounded-full font-bold">
+          Adicionar Transação
+          <ArrowDownUpIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar Transação</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant={"outline"}>Cancelar</Button>
+          <Button>Adicionar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTransactionButton;
